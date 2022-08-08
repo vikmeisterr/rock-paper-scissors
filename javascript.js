942450f3744fdb6d3eb8c83bdad9f4be6bb1939c
vikmeister69 <viktor.ivanovski11@gmail.com>
2022-08-08 12:31:25 +0200

    added functions(player input and computer choice)

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
-console.log("Testing");
+function getComputerChoice(){
+    let computerChoice = Math.random()
+
+        if (computerChoice<0.34){
+            computerChoice = "rock";
+        }
+        else if (computerChoice<=0.67){
+            computerChoice = "paper";
+        }
+        else{
+            
+            computerChoice = "scissors";
+        }
+        return computerChoice;
+    
+}
+
+function playerChoice(){
+    let choice = prompt('write rock, paper or scissors', ``);
+    if (choice == `rock`)
+        return true;
+    else if(choice == `paper`)
+        return true;
+    else if(choice == `scissors`)
+        return true;
+    else
+        return false;
+    
+}
+alert(playerChoice());
+
